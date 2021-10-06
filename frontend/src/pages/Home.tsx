f1a1716137354f7664552169e2fdba0bee2e3922
tasuku-lab280 <loweng280@gmail.com>
2021-10-07 00:21:28 +0900

    旧ディレクトリ削除

--- frontend/src/pages/Home.tsx
+++
@@ -1,5 +0,0 @@
-const Home = () => {
-  return <div>aaaaaaaaaaaaaaa</div>;
-};
-
-export default Home;
--- frontend/src/pages/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as Home } from "./Home";
-export { default as MyPost } from "./mypage/Post";
