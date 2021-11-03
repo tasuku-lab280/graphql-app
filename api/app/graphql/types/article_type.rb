4fc53aa5d947a8aff8fc356e8d1d83983286b1bc
tasuku-lab280 <loweng280@gmail.com>
2021-11-04 00:10:23 +0900

    delete: 不要ファイル削除

--- api/app/graphql/types/article_type.rb
+++
@@ -1,8 +0,0 @@
-module Types
-  class ArticleType < Types::BaseObject
-    field :id, ID, null: false
-    field :status_text, String, null: true
-    field :title, String, null: true
-    field :body, String, null: true
-  end
-end
