0698809407ee445569d7d8756f0db04c3189491c
tasuku-lab280 <loweng280@gmail.com>
2021-11-03 22:43:28 +0900

    feat-client: 投稿API疎通まで

--- client/src/services/auth0/Auth0Provider.tsx
+++ client/src/services/auth0/Auth0Provider.tsx
@@ -1,17 +1,19 @@
 import { Auth0Provider as Provider } from "@auth0/auth0-react";
 
-const domain = process.env.NEXT_PUBLIC_AUTH0_DOMAIN;
-const clientId = process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID;
+const domain = process.env.NEXT_PUBLIC_AUTH0_DOMAIN || "";
+const clientId = process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID || "";
+const audience = process.env.NEXT_PUBLIC_API_BASE_URL || "";
 const redirectUri = process.env.NEXT_PUBLIC_BASE_URL;
 
-const Auth0Provider = (props: { children: React.ReactNode }) => {
+const Auth0Provider: React.FC = ({ children }) => {
   return (
     <Provider
-      domain={domain as string}
-      clientId={clientId as string}
+      domain={domain}
+      clientId={clientId}
+      audience={audience}
       redirectUri={redirectUri}
     >
-      {props.children}
+      {children}
     </Provider>
   );
 };
