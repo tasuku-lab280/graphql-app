52275f9ccedac513a277c83480de3fe7c6e71156
tasuku-lab280 <loweng280@gmail.com>
2021-11-13 22:48:12 +0900

    feat: material ui導入

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -3,12 +3,15 @@ import "../styles/globals.css";
 
 import Auth0Provider from "../services/auth0/Auth0Provider";
 import ApolloProvider from "../services/graphql/ApolloProvider";
+import ThemeProvider from "../services/theme/ThemeProvider";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Auth0Provider>
       <ApolloProvider>
-        <Component {...pageProps} />
+        <ThemeProvider>
+          <Component {...pageProps} />
+        </ThemeProvider>
       </ApolloProvider>
     </Auth0Provider>
   );
