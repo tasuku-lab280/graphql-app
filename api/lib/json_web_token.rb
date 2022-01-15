2f6f6e6ee4ca9d9eed95a0156b53a234a6c9dd4e
tasuku-lab280 <loweng280@gmail.com>
2022-01-15 18:23:21 +0900

    fix-api: JWT修正

--- api/lib/json_web_token.rb
+++ api/lib/json_web_token.rb
@@ -7,7 +7,7 @@ class JsonWebToken
     JWT.decode(token, nil,
               true, # Verify the signature of this token
               algorithm: 'RS256',
-              iss: ENV['AUTH0_DOMAIN'],
+              iss: "https://#{ENV['AUTH0_DOMAIN']}/",
               verify_iss: true,
               aud: ENV['AUTH0_IDENTIFIER'],
               verify_aud: true) do |header|
@@ -16,7 +16,7 @@ class JsonWebToken
   end
 
   def self.jwks_hash
-    jwks_raw = Net::HTTP.get URI("#{ENV['AUTH0_DOMAIN']}.well-known/jwks.json")
+    jwks_raw = Net::HTTP.get URI("https://#{ENV['AUTH0_DOMAIN']}/.well-known/jwks.json")
     jwks_keys = Array(JSON.parse(jwks_raw)['keys'])
     Hash[
       jwks_keys
